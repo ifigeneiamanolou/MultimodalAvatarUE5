57e78a5b6e4ab4c781bdd2370d3022e1642417e1
Ifigeneia Manolou <ipm33@bath.ac.uk>
2026-07-28 13:14:09 +0300

    fixed compilation errors

--- Source/AvatarProject/Private/MyGameInstance.cpp
+++ Source/AvatarProject/Private/MyGameInstance.cpp
@@ -10,16 +10,19 @@
 #include "Kismet/GameplayStatics.h"
 #include "Misc/Optional.h" // TOptional
 #include "json.hpp"
+#include "Windows/AllowWindowsPlatformTypes.h"
 #include "winsock2.h"
+#include "Windows/HideWindowsPlatformTypes.h"
 #include "sys/types.h"
 #include <map>
 #include<iostream>
 #define MAX_BUFF_SIZE 10
 
 using json = nlohmann::json;
+using namespace std;
 
 struct AudioHeaderMsg {
-	std::string type;
+	string type;
 	int sentence_id;
 	int chunk_index;
 	int length;
@@ -27,16 +30,16 @@ struct AudioHeaderMsg {
 
 
 struct EmotionMsg {
-	std::string type;
+	string type;
 	int sentence_id;
-	std::string sentence;
-	std::string emotion;
-	std::map<std::string, float> predictions;
+	string sentence;
+	string emotion;
+	map<string, float> predictions;
 	float maxProb;
 };
 
 struct AudioEndMsg {
-	std::string type;
+	string type;
 	int sentence_id;
 };
 
@@ -58,11 +61,13 @@ void UMyGameInstance::Init()
 		UE_LOG(LogTemp, Display, TEXT("Successfully started server on port 7865 !"));
 
 		// Handle client connection
-		struct sockaddr_in* client = reinterpret_cast<sockaddr_in*>(connectedSocket->GetRemoteAddr());
+		sockaddr_in* client = connectedSocket->GetRemoteAddr();
+		FString ipv4 = "";   // client ipv4 address
+		uint16 port = 0;     // port of client
 		if (client) {
-			FString ipv4 = FString(ANSI_TO_TCHAR(inet_ntoa(client->sin_addr)));
-			uint16 port = ntohs(client->sin_port);
-			UE_LOG(LogTemp, Display, TEXT("Client connected with ipv4 %s and port %d !", ipv4, port));
+			ipv4 = ANSI_TO_TCHAR(inet_ntoa(client->sin_addr));
+			port = ntohs(client->sin_port);
+			UE_LOG(LogTemp, Display, TEXT("Client connected with ipv4 %s and port %d !"), *ipv4, port);
 		}
 
 		FNetWebSocketPacketReceivedCallBack messageCallback;
@@ -75,14 +80,14 @@ void UMyGameInstance::Init()
 
 			// Otherwise parse incoming JSON string
 			FString message = UTF8_TO_TCHAR(static_cast<const char*>(data));
-			std::string input = std::string(TCHAR_TO_UTF8(*message));
+			string input = string(TCHAR_TO_UTF8(*message));
 			if (input == "[[DONE]]") {
 				UE_LOG(LogTemp, Display, TEXT("stream complete !"));
 				if (ActiveSequenceId != -1) {
 					endAudio(GetWorld());
 					ActiveSequenceId = -1;
 				};
-				this->produceEmotion(-1, std::map<std::string, float>());
+				this->produceEmotion(-1, map<string, float>());
 				this->handleAudioEnd();
 
 				return;
@@ -97,7 +102,7 @@ void UMyGameInstance::Init()
 			}
 
 			// Extract the type of the message
-			const std::string type = parsed.value("type", "");
+			const string type = parsed.value("type", "");
 			if (type == "emotion") {
 				UE_LOG(LogTemp, Display, TEXT("Emotion received"));
 				try {
@@ -105,7 +110,7 @@ void UMyGameInstance::Init()
 					this->produceEmotion(msg.sentence_id, msg.predictions);
 				}
 				catch (const json::exception& e) {		// Handle missing key or type mismatch
-					UE_LOG(LogTemp, Warning, TEXT("Invalid json %s !", UTF8_TO_TCHAR(e.what())));
+					UE_LOG(LogTemp, Warning, TEXT("Invalid json %s !"), UTF8_TO_TCHAR(e.what()));
 				};
 			}
 			else if (type == "audio_chunk") {
@@ -115,7 +120,7 @@ void UMyGameInstance::Init()
 					this->handleHeader(msg.sentence_id, msg.chunk_index, msg.length);
 				} 
 				catch (const json::exception& e) {		// Handle missing key or type mismatch
-					UE_LOG(LogTemp, Warning, TEXT("Invalid json %s !", UTF8_TO_TCHAR(e.what())));
+					UE_LOG(LogTemp, Warning, TEXT("Invalid json %s !"), UTF8_TO_TCHAR(e.what()));
 				};
 
 			}
@@ -126,7 +131,7 @@ void UMyGameInstance::Init()
 					this->handleEnd(msg.sentence_id);
 				}
 				catch (const json::exception& e) {		// Handle missing key or type mismatch
-					UE_LOG(LogTemp, Warning, TEXT("Invalid json %s !", UTF8_TO_TCHAR(e.what())));
+					UE_LOG(LogTemp, Warning, TEXT("Invalid json %s !"), UTF8_TO_TCHAR(e.what()));
 				};
 			}
 			else {
@@ -136,16 +141,15 @@ void UMyGameInstance::Init()
 
 		// Handle connection errors
 		FNetWebSocketInfoCallBack errorCallback;
-		errorCallback.BindLambda([](void* data) {
-			UE_LOG(LogTemp, Display, TEXT("error from client with ipv4 %s and port %d !", ipv4, port));
-			});
+		errorCallback.BindLambda([ipv4, port]() {
+			UE_LOG(LogTemp, Display, TEXT("error from client with ipv4 %s and port %d !"), *ipv4, port);
+		});
 
 		// Handle the event a client disconnects from the server
 		FNetWebSocketInfoCallBack closeCallback;
-		closeCallback.BindLambda([](void* data) {
-			UE_LOG(LogTemp, Display, TEXT("client with ipv4 %s and port %d disconnected from the server !", ipv4, port));
-
-			});
+		closeCallback.BindLambda([ipv4, port]() {
+			UE_LOG(LogTemp, Display, TEXT("client with ipv4 %s and port %d disconnected from the server !"), *ipv4, port);
+		});
 
 		connectedSocket->SetReceiveCallBack(messageCallback);
 		connectedSocket->SetErrorCallBack(errorCallback);
@@ -170,7 +174,7 @@ void UMyGameInstance::handleAudioEnd() {
 };
 
 // Emotion queue producer
-void UMyGameInstance::produceEmotion(int id, std::map<std::string, float> emotions) {
+void UMyGameInstance::produceEmotion(int id, map<string, float> emotions) {
 	// format emotion item
 	FEmotion emotion = {
 		id,
